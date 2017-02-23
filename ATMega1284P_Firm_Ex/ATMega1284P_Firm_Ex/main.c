cc7fe76149b73c3ac329c3f7c9b69e1ab15d7091
Patrick Dunham <patrick.dunham@uconn.edu>
2017-02-23 10:34:04 -0500

    Add Atmel Studio version of bootloader
    Adds an Atmel Studio project containing the example bootloader code. Due
    to the lack of WinAVR's odd error, the startup sequence is slightly more
    robust. In addtion, adds a working copy of example firmware. ALL FUTURE
    FIRMWARE MUST DISABLE THE WDT IF NOT USED. This is critical for proper
    firmware execution.

--- ATMega1284P_Firm_Ex/ATMega1284P_Firm_Ex/main.c
+++ ATMega1284P_Firm_Ex/ATMega1284P_Firm_Ex/main.c
@@ -21,33 +21,42 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+//#include "uart.h"
 
 
 /*** FUNCTION DECLARATIONS ***/
 
+void disableWDT(void);
 
 
 /*** VARIABLES & DEFINITIONS ***/
 
-#define LED PINB0
-
+#define LED_PIN PINB0
+#define LED_PERIOD 1000
 
 /*** Code ***/
 
 int main(void) {
 	/* Setup & Initialization */
 	
-	DDRB |= (1<<LED);
+	// Cleans up from bootloader exit.
+	cli();
+	disableWDT();
+	
+	// Sets LED Pin as an output.
+	DDRB |= (1<<LED_PIN);
 
     while (1) {
 		/* Loop */
 		
-		PORTB ^= (1<<LED);
-		_delay_ms(500);
+		// Toggles LED state when needed
+		PORTB ^= (1<<LED_PIN);
+		_delay_ms(LED_PERIOD / 2);
 		
     }
 }
-
 
 
 /*** Interrupt Service Routines ***/
@@ -55,3 +64,16 @@ int main(void) {
 
 
 /*** Function Bodies ***/
+
+// Disables Watchdog Timer, ensuring firmware runs properly.
+void disableWDT(void) {
+	// Make sure we reset the timer. Don't want to get caught in a loop! 
+	wdt_reset();
+	
+	// First, we clear the WDT Reset flag. This prevents any overwriting of our WDT settings.
+	MCUSR &= ~(1<<WDRF);
+	
+	// Then, we clear the WDT Control register. Now it's off for good.
+	WDTCSR |= (1<<WDCE)|(1<<WDE);
+	WDTCSR = 0x00;
+}
---
+++ ATMega1284_Boot/ATMega1284_Boot/uart.c
@@ -0,0 +1,137 @@
+/*
+ * UART driver code.
+ */
+
+#include <avr/io.h>
+#include "uart.h"
+
+#define  F_CPU 20000000
+#define  BAUD 11520
+
+
+/* init UART1
+ * BAUD must be set and setbaud imported before calling this
+ */
+void UART1_init(void)
+{
+    #include <util/setbaud.h>
+    UBRR1H = 0; // Set the baud rate
+    UBRR1L = 10;
+
+    //#if USE_2X
+    //UCSR1A |= (1 << U2X1);
+    //#else
+    UCSR1A &= ~(1 << U2X1);
+    //#endif
+
+    UCSR1B = (1 << RXEN1) | (1 << TXEN1); // Enable receive and transmit
+
+    // Use 8-bit character sizes
+    UCSR1C = (1 << UCSZ11) | (1 << UCSZ10);
+}
+
+void UART1_putchar(unsigned char data)
+{
+    while(!(UCSR1A & (1 << UDRE1)))
+    {
+        // Wait for the last bit to send.
+    }
+    UDR1 = data;
+}
+
+bool UART1_data_available(void)
+{
+    return (UCSR1A & (1 << RXC1)) != 0;
+}
+
+unsigned char UART1_getchar(void)
+{
+    while (!UART1_data_available())
+    {
+        /* Wait for data to be received */
+    }
+    /* Get and return received data from buffer */
+    return UDR1;
+}
+
+void UART1_flush(void)
+{
+    // Tell the compiler that this variable is not being used
+    unsigned char __attribute__ ((unused)) dummy;  // GCC attributes
+    while ( UART1_data_available() ) dummy = UDR1;
+}
+
+void UART1_putstring(char* str)
+{
+    int i = 0;
+    while(str[i] != 0){
+        UART1_putchar(str[i]);
+        i += 1;
+    }
+    UART1_putchar((unsigned char)0);  // make sure we send out the null terminator
+}
+
+
+/* init UART0
+ * BAUD must be set and setbaud imported before calling this
+ */
+void UART0_init(void) {
+    #include <util/setbaud.h>
+    UBRR0H = 0; // Set the baud rate
+    UBRR0L = 10;
+
+    //#if USE_2X
+    //UCSR0A |= (1 << U2X0);
+    //#else
+    UCSR0A &= ~(1 << U2X0);
+    //#endif
+
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0); // Enable receive and transmit
+
+    // Use 8-bit character sizes
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void UART0_putchar(unsigned char data)
+{
+    while(!(UCSR0A & (1 << UDRE0)))
+    {
+        // Wait for the last bit to send
+    }
+    UDR0 = data;
+}
+
+bool UART0_data_available(void)
+{
+    return (UCSR0A & (1 << RXC0)) != 0;
+}
+
+unsigned char UART0_getchar(void)
+{
+    while(!UART0_data_available())
+    {
+        /* Wait for data to be received */
+    }
+    /* Get and return received data from buffer */
+    return UDR0;
+}
+
+void UART0_flush(void)
+{
+    // Tell the compiler that this variable is not being used
+    unsigned char __attribute__ ((unused)) dummy;  // GCC attributes
+    while(UART0_data_available())
+    {
+        dummy = UDR0;
+    }
+}
+
+void UART0_putstring(char* str)
+{
+    int i = 0;
+    while(str[i] != 0){
+        UART0_putchar(str[i]);
+        i += 1;
+    }
+    UART0_putchar((unsigned char)0);  // make sure we send out the null terminator
+}
