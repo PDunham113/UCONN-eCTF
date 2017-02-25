f64d3214363d2b20fe8ed731618a365ef4ab5ed2
lwm14001 <luke.malinowski@uconn.edu>
2017-02-25 17:08:31 -0500

    AES now has a generic encrypt with CBC and fw_protect has the good stuff.

--- AESandRandNums.py
+++ AESandRandNums.py
@@ -19,7 +19,16 @@ def encryptFileAES(key, iv, input_file, output_file):
                 elif len(chunk) % 16 != 0:
                     chunk += b' ' * (16-len(chunk)%16)
                 outfile.write(encryptor.encrypt(chunk))
-            
+def encryptBlockAES(key,input_file,output_file):
+    encryptor = AES.new(key,AES.MODE_ECB)
+    with open(input_file, 'rb') as infile:
+        with open(output_file,'wb') as outfile:
+            block = infile.read(16)
+            if len(block) == 0:
+                pass
+            elif len(block) % 16 != 0:
+                block += b'\x00'* (16 - len(block)%16)
+            outfile.write(encryptor.encrypt(block))
 def decryptFileAES(key, iv, input_file, output_file):
     """ Takes in a key, initialization vector, and a file location of the input, and location of the output"""
     decryptAES = AES.new(key, AES.MODE_CFB, iv)
