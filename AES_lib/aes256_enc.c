860944e29b22714cf9171193cbd8816af5065c23
BrianMarquis <bdm2195@gmail.com>
2017-02-25 14:14:30 -0500

    Merge remote-tracking branch 'origin/bootloader_dev' into bootloader_dev
    # Conflicts:
    #	ATMega1284_Boot/.vs/ATMega1284_Boot/v14/.atsuo
    #	ATMega1284_Boot/ATMega1284_Boot/ATMega1284_Boot.cproj

---
+++ AES_lib/aes256_enc.c
@@ -0,0 +1,36 @@
+/* aes256_enc.c */
+
+
+#include "aes.h"
+#include "aes_enc.h"
+#include <avr/pgmspace.h>
+
+/*  This is the AES RSM 256 encryption function that call the generic AES RSM encryption core*/
+
+void aes256_enc(uint8_t* j, void* buffer, aes256_ctx_t* ctx){
+	aes_encrypt_core(j,buffer, (aes_genctx_t*)ctx, 14);
+}
+
+
+/*This is the AES RSM 256 sequencer that initialize and launches the encryption
+ *It output the cyphertext on the serial output  */
+/*Inputs : 	v 	: Pointer to first byte of the plaintext buffer
+ *			k 	: Pointer to first byte of the key
+ *			j  	: Pointer to the output of the random offset
+ *			rng : Flag that enables the Trigger signal during AES encryption when = 1
+ *
+ *  */
+void aes_cenc(uint8_t *v, uint8_t *k,uint8_t *j) {
+	aes256_ctx_t ctx;
+	uint8_t i = 0;
+	uint8_t tmp=j[0];
+	//Initialization
+
+	aes256_init(k, &ctx);
+
+	//Encryption
+	aes256_enc(j, v, &ctx);
+}
+
+
+
