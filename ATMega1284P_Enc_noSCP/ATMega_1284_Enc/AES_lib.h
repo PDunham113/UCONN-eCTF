624f7b6568c4d787aa7b02e2e580e65a81d2112e
Patrick Dunham <patrick.dunham@uconn.edu>
2017-02-26 12:54:54 -0500

    Drop encryption functions in library

---
+++ ATMega1284P_Enc_noSCP/ATMega_1284_Enc/AES_lib.h
@@ -0,0 +1,20 @@
+/*
+ * AES_lib.h
+ *
+ * Created: 2/26/2017 12:49:46 PM
+ *  Author: Patrick Dunham
+ */ 
+
+
+#ifndef AES_LIB_H_
+#define AES_LIB_H_
+
+#include "AES_lib/aes.h"
+
+
+
+void encCFB(uint8_t* key, uint8_t* data, uint8_t* IV, uint16_t size);
+void decCFB(uint8_t* key, uint8_t* data, uint8_t* IV, uint16_t size);
+void hashCBC(uint8_t* key, uint8_t* data, uint8_t* hash, uint16_t size);
+
+#endif /* AES_LIB_H_ */
