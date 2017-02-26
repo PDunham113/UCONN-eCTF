33c4d56684d232bb32a2b2543e3d1559d6e9900f
lwm14001 <luke.malinowski@uconn.edu>
2017-02-25 19:10:39 -0500

    fixed segment_size=128

--- AESandRandNums.py
+++ AESandRandNums.py
@@ -9,7 +9,7 @@ import os, struct
 
 def encryptFileAES(key, iv, input_file, output_file):
     """ Takes in a key, initialization vector, and a file location of the input, and location of the output"""
-    encryptor = AES.new(key, AES.MODE_CFB, iv)
+    encryptor = AES.new(key, AES.MODE_CFB, iv,segment_size=128)
     with open(input_file, 'rb') as infile:
         with open(output_file, 'wb') as outfile:
             while True:
@@ -19,8 +19,8 @@ def encryptFileAES(key, iv, input_file, output_file):
                 elif len(chunk) % 16 != 0:
                     chunk += b' ' * (16-len(chunk)%16)
                 outfile.write(encryptor.encrypt(chunk))
-def encryptBlockAES(key,input_file,output_file):
-    encryptor = AES.new(key,AES.MODE_ECB)
+def AESHash(key,iv,input_file,output_file):
+    encryptor = AES.new(key,AES.MODE_CBC,iv,segment_size=128)
     with open(input_file, 'rb') as infile:
         with open(output_file,'wb') as outfile:
             block = infile.read(16)
@@ -28,10 +28,11 @@ def encryptBlockAES(key,input_file,output_file):
                 pass
             elif len(block) % 16 != 0:
                 block += b'\x00'* (16 - len(block)%16)
+                print("heh")
             outfile.write(encryptor.encrypt(block))
 def decryptFileAES(key, iv, input_file, output_file):
     """ Takes in a key, initialization vector, and a file location of the input, and location of the output"""
-    decryptAES = AES.new(key, AES.MODE_CFB, iv)
+    decryptAES = AES.new(key, AES.MODE_CFB, iv,segment_size=128)
     with open(input_file, 'rb') as infile:
         with open(output_file, 'wb') as outfile:
             while True:
