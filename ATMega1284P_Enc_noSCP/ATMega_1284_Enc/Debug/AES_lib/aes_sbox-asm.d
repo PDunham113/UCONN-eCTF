2cd247b85b287441a9ee060579b18ff7ea4a6605
Patrick Dunham <patrick.dunham@uconn.edu>
2017-02-26 20:42:46 -0500

    Adding makefiles errywhere

---
+++ ATMega1284P_Enc_noSCP/ATMega_1284_Enc/Debug/AES_lib/aes_sbox-asm.d
@@ -0,0 +1,1 @@
+AES_lib/aes_sbox-asm.d AES_lib/aes_sbox-asm.o: ../AES_lib/aes_sbox-asm.S
---
+++ ATMega1284P_Enc_noSCP/ATMega_1284_Enc/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+AES_lib.c
+
+AES_lib\aes_enc-asm.S
+
+AES_lib\aes_keyschedule-asm.S
+
+AES_lib\aes_sbox-asm.S
+
+AES_lib\avr-asm-macros.S
+
+AES_lib\keysize_descriptor.c
+
+main.c
+
+uart.c
+
