000c0d05ca73a5a3487a5cfe66dfb8d7448f350a
lwm14001 <luke.malinowski@uconn.edu>
2017-02-28 11:55:45 -0500

    updated bl_build to be much more compact

--- host_tools/bl_build.py
+++ host_tools/bl_build.py
@@ -21,42 +21,6 @@ def bytesToHexList(data):
     return ["\\x{}".format(b) for b in data_new]
 def bytesToCString(data):
     return "".join(bytesToHexList(data))
-def encryptFileAES(key, iv, input_file, output_file):
-    """ Takes in a key, initialization vector, and a file location of the input, and location of the output"""
-    encryptor = AES.new(key, AES.MODE_CFB, iv,segment_size=128)
-    with open(input_file, 'rb') as infile:
-        with open(output_file, 'wb') as outfile:
-            while True:
-                chunk = infile.read(16)
-                if len(chunk) == 0:
-                    break
-                elif len(chunk) % 16 != 0:
-                    chunk += b' ' * (16-len(chunk)%16)
-                outfile.write(encryptor.encrypt(chunk))
-def AESHash(key,iv,input_file,output_file):
-    encryptor = AES.new(key,AES.MODE_CBC,iv,segment_size=128)
-    with open(input_file, 'rb') as infile:
-        with open(output_file,'wb') as outfile:
-            output = None
-            while True:
-                block = infile.read(16)
-                if len(block) == 0:
-                    break
-                elif len(block) % 16 != 0:
-                    block += b'\x00'* (16 - len(block)%16)
-                output = encryptor.encrypt(block)
-            outfile.write(output)
-def decryptFileAES(key, iv, input_file, output_file):
-    """ Takes in a key, initialization vector, and a file location of the input, and location of the output"""
-    decryptAES = AES.new(key, AES.MODE_CFB, iv,segment_size=128)
-    with open(input_file, 'rb') as infile:
-        with open(output_file, 'wb') as outfile:
-            while True:
-                chunk = infile.read(16)
-                if len(chunk)==0:
-                    break
-                outfile.write(decryptAES.decrypt(chunk))
-
 def generate128Entropy():
     return os.urandom(16) 
 def generate256Entropy():
@@ -96,19 +60,6 @@ def copy_artifacts():
     shutil.copy(os.path.join(src_dir, 'flash.hex'), dst_dir)
     shutil.copy(os.path.join(src_dir, 'eeprom.hex'), dst_dir)
 
-def generate_readback_password():
-    """
-    Generate secret password for readback tool and store to secret file.
-    """
-    # Generate 16 character random password.
-    pw = ''.join(chr(random.randint(0, 255)) for i in range(8)).encode('hex')
-
-    # Write password to secret file.
-    with open('secret_build_output.txt', 'wb+') as secret_build_output:
-        secret_build_output.write(pw)
-
-    return pw
-
 def write_fuse_file(fuse_name, fuse_value):
     hex_file = IntelHex()
     hex_file[0] = fuse_value
@@ -116,7 +67,9 @@ def write_fuse_file(fuse_name, fuse_value):
     with open(os.path.join(FILE_DIR, fuse_name + '.hex'), 'wb+') as outfile:
         hex_file.tofile(outfile, format='hex')
 
+#generate the keys and Password
 def generate_secrets():
+
     FW_KEY = generate256Entropy()
     RB_KEY = generate256Entropy()
     H_KEY = generate256Entropy()
@@ -144,16 +97,10 @@ if __name__ == '__main__':
     secrets = generate_secrets()
     make_secrets_file(secrets)
     password = generate_readback_password()
-    if not make_bootloader(password=password):
+    if not make_bootloader():
         print "ERROR: Failed to compile bootloader."
         sys.exit(1)
     copy_artifacts()
     write_fuse_file('lfuse', 0xFF)
     write_fuse_file('hfuse', 0x18)
     write_fuse_file('efuse', 0xFC)
-
-
-# In[ ]:
-
-
-
