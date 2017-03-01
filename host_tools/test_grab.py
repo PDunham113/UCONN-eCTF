3192c6ae28a618355d0648c2e9bf2ef8cc4b38db
lwm14001 <luke.malinowski@uconn.edu>
2017-02-28 20:25:36 -0500

    added hash map for key value pairs

--- host_tools/test_grab.py
+++ host_tools/test_grab.py
@@ -1,14 +1,17 @@
 import struct
-with open("secret_build_output.txt",'r') as keyFile:
-    y = keyFile.readline()
-    while len(y) > 5:
-        y = y.split(" ")
-        z = y[2]
-        z=z[1:-2]
-        key = []
-        for i in range(len(z)//4):
-                key.append(struct.pack(">B",int(z[4*i+2:4*i+4],16)))
-        key = b''.join(key)
-        print(type(key))
-        print([hex(ord(x)) for x in key])
+def grabKeys():
+    with open("secret_build_output.txt",'r') as keyFile:
         y = keyFile.readline()
+        while len(y) > 5:
+            y = y.split(" ")
+            z = y[2]
+            z=z[1:-2]
+            keyValues = {}
+            key = []
+            for i in range(len(z)//4):
+                    key.append(struct.pack(">B",int(z[4*i+2:4*i+4],16)))
+            key = b''.join(key)
+            keyValues[y[1]] = key
+            y = keyFile.readline()
+        return keyValues
+    
